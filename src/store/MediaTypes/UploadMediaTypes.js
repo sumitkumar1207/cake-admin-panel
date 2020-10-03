9dc27c952090330affcd247db73a5db1721c9852
sumitkumar <sumit@binarynumbers.io>
2020-10-03 08:59:43 +0530

    Dynamic types of cake file created.

---
+++ src/store/MediaTypes/UploadMediaTypes.js
@@ -0,0 +1,8 @@
+module.exports = {
+  COMMON_FAIL_MESSAGE: "Sorry! Something went wrong. kindly contact your administrator",
+  REST_RESPONSE_MESSAGE: "REST_RESPONSE_MESSAGE",
+  /**
+   *  Upload Image common type:
+   */
+  UPLOAD_COMMON_IMAGE: "UPLOAD_COMMON_IMAGE",
+};
