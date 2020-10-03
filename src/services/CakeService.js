7a6940e6381dae2ea74ff8524ab6fa803c08bf46
sumitkumar <sumit@binarynumbers.io>
2020-10-03 14:15:38 +0530

    Service created for apis of cakes.

--- src/services/CakeService.js
+++ src/services/CakeService.js
@@ -4,4 +4,17 @@ export default {
   AddCake(payload) {
     return Api().post(`/app/cake/add-cake`, payload);
   },
+  UpdateCake(payload) {
+    return Api().put(`/app/cake/edit-cake/${payload.cake_id}`, payload)
+  },
+  DeleteCake(payload) {
+    return Api().delete(`/app/cake/delete-cake/${payload.cake_id}`, payload)
+  },
+  GetCakes(payload) {
+    let url = `/app/cake/get-cakes`;
+    if (payload) {
+      url += `?limit=${payload['limit']}&currentPage=${payload['currentPage']}`
+    };
+    return Api().get(`${url}`)
+  },
 }
