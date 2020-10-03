71f79fd6c4b88910e0fa38ee522af3134fe424d8
sumitkumar <sumit@binarynumbers.io>
2020-10-03 15:15:14 +0530

    Unit types created.

---
+++ src/store/AdminTypes/unitTypes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  COMMON_FAIL_MESSAGE: "Sorry! Something went wrong. kindly contact your administrator",
+  REST_RESPONSE_MESSAGE: "REST_RESPONSE_MESSAGE",
+  /**
+   *  Cake:
+  */
+  ADD_UNITS: "ADD_UNITS",
+  GET_UNITS: "GET_UNITS",
+};
