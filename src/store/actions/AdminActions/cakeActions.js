4e7327640a690bca8457b284d64ce64aa853211b
sumitkumar <sumit@binarynumbers.io>
2020-10-03 14:16:06 +0530

    Update delete and list of cakes apis action created.

--- src/store/actions/AdminActions/cakeActions.js
+++ src/store/actions/AdminActions/cakeActions.js
@@ -16,3 +16,40 @@ export const AddCake = (payload) => async dispatch => {
     dispatch({ type: cakeTypes.COMMON_FAIL_MESSAGE, message: "Sorry! Something went wrong. kindly contact your administrator" })
   }
 };
+
+export const updateCake = (payload) => async dispatch => {
+  // console.log('updateCake payload', payload)
+  const { data } = await CakeService.UpdateCake(payload);
+  // console.log('updateCake data', data)
+  if (data && data.status) {
+    await dispatch({ type: "SHOW_SNACKBAR", message: data.message })
+    dispatch(getCakeList())
+  } else {
+    // Handle Error Message Here
+    dispatch({ type: "SHOW_SNACKBAR", message: data.message || cakeTypes.COMMON_FAIL_MESSAGE, time: 3000 });
+  };
+};
+
+export const deleteCake = (payload) => async dispatch => {
+  // console.log('deleteCake payload', payload)
+  const { data } = await CakeService.DeleteCake(payload);
+  // console.log('updateCake data', data)
+  if (data && data.status) {
+    await dispatch({ type: "SHOW_SNACKBAR", message: data.message })
+    dispatch(getCakeList())
+  } else {
+    // Handle Error Message Here
+    dispatch({ type: "SHOW_SNACKBAR", message: data.message || cakeTypes.COMMON_FAIL_MESSAGE, time: 3000 });
+  };
+};
+
+
+export const getCakeList = (payload) => async dispatch => {
+  const { data } = await CakeService.GetCakes(payload);
+  if (data && data.status) {
+    dispatch({ type: cakeTypes.GET_CAKES, Records: data.Records || [] });
+  } else {
+    // Handle Error Message Here
+    dispatch({ type: "SHOW_SNACKBAR", message: data.message || cakeTypes.COMMON_FAIL_MESSAGE });
+  };
+};
